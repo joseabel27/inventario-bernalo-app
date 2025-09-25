26228edda40760f00a5fa952625ff104cb883256
Jose Romero <parrajose27@hotmail.com>
2025-09-25 13:16:17 -0500

    agregar productos

--- src/inventario.js
+++ src/inventario.js
@@ -1,16 +1,14 @@
-let inventario = [
-    {
-        id: 1, nombre: "Bascula Bernalo 150kg", categoria:"Basculas", precio: 392700, cantidad:500}
-];
+let inventario = [];
 
 
+/* FUNCION PARA LISTAR TODOS LOS PRODUCTOS */
 
 function listarProductos(){
 
     console.log("Inventario Actual:");
 
     /* SI NO HAY PRODUCTOS, MOSTRAMOS UN MSJ */
-    if (inventario.legth === 0){
+    if (inventario.length === 0){
     console.log("No hay productos en el inventario.");
     return;
 }
@@ -19,11 +17,36 @@ function listarProductos(){
 /* SI HAY PRODUCTOS, LO RECORREMOS UNO POR UNO */
 
 inventario.forEach((producto)=>{
-    console.log(`ID: ${producto.id} | Nombre: ${producto.nombre} | Categoria: ${producto.categoria} | Precio: ${producto.precio}| Cantidad: ${producto.cantidad}`
+    console.log(`ID: ${producto.id} | Nombre: ${producto.nombre} | Categoria: ${producto.categoria} | Precio: ${producto.precio}| Cantidad: ${producto.cantidad} | Ubicacion: ${producto.ubicacion}`
     );
 });
 }
 
 
+/* FUNCION PARA AGREGAR UN PRODUCTO */
+
+function agregarProductos(nombre, cantidad, categoria, ubicacion, precio){
+
+    /* CREAMOS UN OBEJTO CON LOS DATOS DEL PRODUCTO */
+
+    const producto = {
+
+        id: inventario.lenght + 1, // ID AUTOMATICO
+        nombre : nombre,
+        cantidad : cantidad,
+        categoria : categoria,
+        ubicacion: ubicacion,
+        precio:precio
+    };
+
+    /* LO AGREGAMOS AL INVENTARIO */
+
+    inventario.push(producto);
+    console.log(`Producto agregado : ${nombre}`);
+}
+
+
 
 module.exports = {listarProductos};
+
+
