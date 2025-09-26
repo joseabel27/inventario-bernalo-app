cce95574b6def89f710f5da07643711580fe17da
Jose Romero <parrajose27@hotmail.com>
2025-09-26 08:23:23 -0500

    modiicando el inventario

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ agregarProductos("Bascula 150kg bernalo wifi","Basculas",400.000, 1000, "Estante
 agregarProductos("Bascula 150kg bernalito pedestal","Basculas", 250.700, 100, "Estante A3");
 agregarProductos("Bascula 150kg bernalito wifi","Basculas", 300.700, 500, "Estante B1");
 agregarProductos("Balanza Precision 3000g","Balanzas", 600.700, 500, "Estante B2");
-agregarProductos("Balanza Precision 3000g","Balanzas", 600.700, 500, "Estante B2");
+agregarProductos("Balanza Precision 600g","Balanzas", 300.000, 500, "Estante B3");
 
 
 /* LISTAMOS EL INVENTARIO */
